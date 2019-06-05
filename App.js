5d31b78122826f6954ea16d1739569c8a683f81a
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-06-05 20:55:38 -0300

    Preparação para uso Redux.

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import DeckList from './components/DeckList';
 import DeckDetail from './components/DeckDetail';
 import Quiz from './components/Quiz';
 import NewDeck from './components/NewDeck';
 import NewCard from './components/NewCard';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import middleware from './middleware';
+import { Constants } from 'expo';
+import { black } from './utils/colors';
+
+function AppStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
 
 export default class App extends React.Component {
+  componentDidMount() {
+    //TODO implementar notificações
+  }
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <DeckList />
-      </View>
+      <Provider store={createStore(reducers, middleware)}>
+        <View style={{ flex: 1 }}>
+          <AppStatusBar backgroundColor={black} barStyle="light-content" />
+          <DeckList />
+        </View>
+      </Provider>
     );
   }
 }
