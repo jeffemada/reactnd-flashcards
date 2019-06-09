a262ccf3f4e4654a3b90ce5d268a0cdae4454940
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-06-09 15:41:35 -0300

    Formatação do arquivo de cores.

--- utils/colors.js
+++ utils/colors.js
@@ -1,6 +1,6 @@
-export const black = '#000'
-export const gray = '#9e9e9e'
-export const green = '#4caf50'
-export const lightGray = '#f5f5f5'
-export const red = '#f44336'
-export const white = '#fff'
+export const black = '#000';
+export const gray = '#9e9e9e';
+export const green = '#4caf50';
+export const lightGray = '#f5f5f5';
+export const red = '#f44336';
+export const white = '#fff';
